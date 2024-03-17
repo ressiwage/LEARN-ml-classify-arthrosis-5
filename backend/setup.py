a7ead71513d96bd064ce5d7ec174ce404aa469f6
rswg <ressiwage@ya.ru>
2024-03-17 21:07:18 +0500

    crazy diamond

--- backend/setup.py
+++ backend/setup.py
@@ -1,8 +1,5 @@
 import gdown
 
-
-
-# same as the above, but with the file ID
 id = "1Cg3i4In6Y9gPcFJahFt7RWh_KoytIbb0"
 gdown.download(id=id, output="65-relative-success-v1.weights.h5", quiet=False)
 
