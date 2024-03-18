40703c75b1d8a37e4df561a073b483815b447670
rswg <ressiwage@ya.ru>
2024-03-18 19:42:26 +0500

    restructure

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Request, Depends
 from fastapi.responses import JSONResponse, HTMLResponse
-from json import JSONDecodeError
-import aiofiles
+from fastapi import File, UploadFile
 import os, json
 import numpy as np
 import utils
@@ -10,20 +9,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
-
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-from fastapi import File, UploadFile
-
-# debug: bool = 0, features: bool=0, brightness: int = 1
-def checker(data: str = Form(...)):
-    try:
-       return json.loads(data)
-    except JSONDecodeError:
-        raise HTTPException(status_code=400, detail='Invalid JSON data')
 
 @app.post("/predict")
 def predict(file: UploadFile, debug: bool = 0, features: bool=0, brightness: int = 1 ):
@@ -45,9 +33,6 @@ def predict(file: UploadFile, debug: bool = 0, features: bool=0, brightness: int
         "payload":result.get("prediction", "error"),
         "filenames":result.get("debug_imgs", "")
     }, ensure_ascii=False, default=lambda x: x.tolist() if isinstance(x, np.ndarray) else x) 
-
-
-
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "message": "Hello, World!"})
